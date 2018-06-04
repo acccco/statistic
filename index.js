f7e0b3bb993e6dcf3a8c1d35f5a8ce806a10e143
aco <wwsxuan@163.com>
2018-06-04 17:01:27 +0800

    整合统计代码

--- index.js
+++ index.js
@@ -11,15 +11,7 @@ export default class Statistic extends Event {
   constructor() {
     super()
     this.firstPv = true
-    try {
-      this._init()
-    } catch (e) {
-      post(getBaseInfo(), 10, {
-        rpu: document.referrer,
-        errt: e.name,
-        errm: e.message
-      })
-    }
+    this._init()
   }
 
   _init() {
@@ -29,64 +21,27 @@ export default class Statistic extends Event {
     this.$emit('init', this)
 
     event.create(window, 'load', () => {
-      this.firstPv && post(getBaseInfo(), 0, getPageInfo())
+      this.firstPv && this.$emit('windowLoad', getPageInfo()
       setTimeout(() => {
-        post(getBaseInfo(), 1, timing())
+        this.$emit('browserTiming', timing())
       }, 5E3)
     })
     event.create(window, 'unload', () => {
       storage.setData(config.namespaces + '_ppai_' + config.appid, config.pageAccessId, true)
-      post(getBaseInfo(), 3, Object.assign({
-        su: window.location.href,
-        ot: new Date() - config.pvStartTime
-      }, behavior.getBehavior(), acTime.getActiveTime()))
+      this.$emit('windowUnload', Object.assign(behavior.getBehavior(), acTime.getActiveTime()))
     })
-
   }
-}
 
-/**
- * 手动开启 pv 统计
- */
-export function pageStart() {
-  config.pageAccessId = new Date() % 63353
-  config.pageInTime = Math.round(new Date() / 1E3)
-  config.pvStartTime = new Date().valueOf()
-  post(getBaseInfo(), 0, getPageInfo())
-}
+  pageStart() {
+    config.pageAccessId = new Date() % 63353
+    config.pvStartTime = new Date().valueOf()
+    this.$emit('pageStart', getPageInfo())
+  }
 
-/**
- * 手动关闭 pv 并上传该页面的相关数据
- */
-export function pageClose() {
-  storage.setData(config.namespaces + '_ppai_' + config.appid, config.pageAccessId, true)
-  config.parentPageAccessId = config.pageAccessId
-  post(getBaseInfo(), 3, Object.assign({
-    su: window.location.href,
-    ot: new Date() - config.pvStartTime
-  }, behavior.getUserBehavior(), acTime.getUserActiveTime()))
-}
+  pageClose() {
+    storage.setData(config.namespaces + '_ppai_' + config.appid, config.pageAccessId, true)
+    config.parentPageAccessId = config.pageAccessId
+    this.$emit('pageClose', Object.assign(behavior.getUserBehavior(), acTime.getUserActiveTime()))
+  }
 
-/**
- * 手动触发错误统计
- * @param e
- */
-export function postError(e) {
-  post({}, 10, {
-    rpu: document.referrer,
-    errt: e.name,
-    errm: e.message
-  })
-}
-
-/**
- * 自定义上传数据
- * @param arr
- */
-export function customPost(arr) {
-  post(getBaseInfo(), 2, {
-    ot: new Date() - config.pvStartTime,
-    pai: config.pageAccessId,
-    data: arr.map(str => str.replace(/|/g, '**')).join('|')
-  })
 }
